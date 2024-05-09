781ebd56e38a8c8dbeebbe998580132fbf7f9a8a
mirazfaruk <mirazfaruk@gmail.com>
2024-05-09 23:43:07 +0600

    Added functional show all

--- js/phone.js
+++ js/phone.js
@@ -1,26 +1,28 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phone);
-    displayPhones(phones);
+    displayPhones(phones, isShowAll);
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, isShowAll) => {
     // console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
     // clear search result
     phoneContainer.textContent = '';
 
     const showAllContainer = document.getElementById('show-all-container');
-    if (phones.length > 12) {
+    if (phones.length > 12 && !isShowAll) {
         showAllContainer.classList.remove('hidden');
     }
     else {
         showAllContainer.classList.add('hidden');
     }
-
-    phones = phones.slice(0, 12);
+// display only 12 phone and show all 
+    if(!isShowAll){
+        phones = phones.slice(0, 12);
+    }
 
     phones.forEach(phone => {
         console.log(phone)
@@ -29,36 +31,42 @@ const displayPhones = phones => {
 
         phoneCard.innerHTML =
             `<figure><img src="${phone.image}" alt="Shoes" /></figure>
-        <div class="card-body">
-            <h2 class="card-title">${phone.phone_name}</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
-            </div>
-        </div>`;
+            <div class="card-body">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>`;
 
         phoneContainer.appendChild(phoneCard);
     });
     toggleLoadingSpinner(false);
 }
 
-
 // Handle search button
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
     toggleLoadingSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText);
-    loadPhone(searchText);
+    loadPhone(searchText, isShowAll);
 }
 // loadPhone();
 
-const toggleLoadingSpinner = (isLoading) =>{
+// handle loading spinner 
+const toggleLoadingSpinner = (isLoading) => {
     const loadingSpinner = document.getElementById('loading-spinner');
-    if(isLoading){
+    if (isLoading) {
         loadingSpinner.classList.remove('hidden');
     }
-    else{
+    else {
         loadingSpinner.classList.add('hidden');
     }
+}
+
+// Handle show all 
+const handleShowAll = () => {
+    handleSearch(true);
+
 }
