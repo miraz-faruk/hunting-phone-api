e0259d0311703492bad8fb33d1495dcf16c7d1ce
mirazfaruk <mirazfaruk@gmail.com>
2024-05-09 21:27:01 +0600

    Added filtre button at search result

--- js/phone.js
+++ js/phone.js
@@ -1,22 +1,35 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phone);
     displayPhones(phones);
-
 }
 
 const displayPhones = phones => {
     // console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
+    // clear search result
+    phoneContainer.textContent = '';
+
+    const showAllContainer = document.getElementById('show-all-container');
+    if (phones.length > 12) {
+        showAllContainer.classList.remove('hidden');
+    }
+    else {
+        showAllContainer.classList.add('hidden');
+    }
+
+
+    phones = phones.slice(0, 12);
+
     phones.forEach(phone => {
         console.log(phone)
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card bg-gray-300 p-4 shadow-xl`;
 
-        phoneCard.innerHTML = 
-        `<figure><img src="${phone.image}" alt="Shoes" /></figure>
+        phoneCard.innerHTML =
+            `<figure><img src="${phone.image}" alt="Shoes" /></figure>
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -24,9 +37,17 @@ const displayPhones = phones => {
                 <button class="btn btn-primary">Buy Now</button>
             </div>
         </div>`;
-        
+
         phoneContainer.appendChild(phoneCard);
     });
 }
 
+
+// Handle search button
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
 loadPhone();
