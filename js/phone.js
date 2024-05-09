3cebee7b8e3bc6c2cceb0b9c118a48c0e9d68f0c
mirazfaruk <mirazfaruk@gmail.com>
2024-05-10 02:20:32 +0600

    Added functional details of all phone when click the button

--- js/phone.js
+++ js/phone.js
@@ -19,29 +19,55 @@ const displayPhones = (phones, isShowAll) => {
     else {
         showAllContainer.classList.add('hidden');
     }
-// display only 12 phone and show all 
-    if(!isShowAll){
+    // display only 12 phone and show all 
+    if (!isShowAll) {
         phones = phones.slice(0, 12);
     }
 
     phones.forEach(phone => {
-        console.log(phone)
+        // console.log(phone)
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card bg-gray-300 p-4 shadow-xl`;
+        phoneCard.classList = `card bg-gray-400 p-4 shadow-xl`;
 
         phoneCard.innerHTML =
             `<figure><img src="${phone.image}" alt="Shoes" /></figure>
             <div class="card-body">
                 <h2 class="card-title">${phone.phone_name}</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                <div class="card-actions justify-center">
+                    <button onClick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
                 </div>
             </div>`;
 
         phoneContainer.appendChild(phoneCard);
     });
+    // hide loading spinner
     toggleLoadingSpinner(false);
+}
+const handleShowDetail = async (id) => {
+    console.log('clicked show details');
+    // // load individual data 
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+
+    showPhoneDetails(phone);
+}
+
+const showPhoneDetails = (phone) => {
+    console.log(phone);
+    const phoneName = document.getElementById('show-detail-phone-name');
+    phoneName.innerText = phone.name;
+
+    const showDetailContainer = document.getElementById('show-detail-container');
+
+    showDetailContainer.innerHTML = `
+        <img src="${phone.image}" alt="">
+        <p><span>Storage:</span>${phone?.mainFeatures?.storage}</p>
+    `
+
+    // Show modal
+    show_details_modal.showModal();
 }
 
 // Handle search button
